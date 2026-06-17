960f09c8794ed4bb03d2d44ef2f3f28a3c9fe3e6
LaTrouTe11 <latroute@gmail.com>
2026-06-17 01:58:52 -0400

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,19 +1,3 @@
-// ===================================================================
-// 🔐 BASE DE DONNÉES SYNCHRONISÉE NOËL V71.0 - PATH: js/config.js
-// ===================================================================
-window.qbcClusterData = {
-    "global": {
-        "type": "portal",
-        "indexMainTitle": "QBC Multigaming",
-        "indexSubTitle": "Portail d'Accès aux Instances Réseau",
-        "warn": "PREWARNING : ⚠️ Initialisation du Cluster QBC. Aucune coupure signalée.",
-        "title": "Bienvenue sur le Réseau QBC",
-        "desc": "Pilotez vos accès à travers nos clusters de jeux interconnectés. Les informations de reboot, les statuts réseau RMI et les fichiers de configurations de nos instances lues en temps réel.",
-        "portalTheme": "noel",
-        "securityNoCopy": "false",
-        "global_badge": "⚠️ ",
-        "global_warn_text": "Initialisation du Cluster QBC. Aucune coupure signalée."
-    },
     "wurm1": {
         "type": "wurm",
         "name": "Doriath (Monde Principal)",
@@ -38,52 +22,3 @@ window.qbcClusterData = {
         "votes": "12",
         "game_icon": "👑 "
     },
-    "dtd1": {
-        "type": "7dtd",
-        "name": "QBC Survie Alpha (Instance 1)",
-        "ip": "67.216.85.235:26900",
-        "rb": "Automatique toutes les 12 heures",
-        "badge_state": "pack",
-        "wn_text": "Reboot effectué - Cycle de pousse à jour !",
-        "status": "online",
-        "slots": "14/40",
-        "votes": "89",
-        "game_icon": "🧟 "
-    },
-    "dtd2": {
-        "type": "7dtd",
-        "name": "Purgatoire (Instance 2 PvP)",
-        "ip": "67.216.85.235:26910",
-        "rb": "Maintenance rapide chaque vendredi",
-        "badge_state": "stable",
-        "wn_text": "Arène PvP ouverte. Attention aux pillages.",
-        "status": "online",
-        "slots": "0/30",
-        "votes": "5",
-        "game_icon": "💀 "
-    },
-    "avo1": {
-        "type": "avorion",
-        "name": "Quadrant Galactique (Secteur 1)",
-        "ip": "67.216.85.235:27020",
-        "rb": "Tous les lundis à 02:00 AM",
-        "badge_state": "stable",
-        "wn_text": "Secteur stable. Radar longue portée actif.",
-        "status": "online",
-        "slots": "5/50",
-        "votes": "21",
-        "game_icon": "🚀 "
-    },
-    "avo2": {
-        "type": "avorion",
-        "name": "Bordure Extérieure (Secteur 2)",
-        "ip": "67.216.85.235:27021",
-        "rb": "Fluctuant selon la charge du secteur",
-        "badge_state": "maintenance",
-        "wn_text": "Nettoyage des débris spatiaux en cours.",
-        "status": "online",
-        "slots": "1/50",
-        "votes": "3",
-        "game_icon": "🌌 "
-    }
-};
